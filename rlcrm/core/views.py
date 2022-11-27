fde1f3529bd5a9259b54b116e6d7aac8a01eba25
Vlad Jianu <jianuvladutz@gmail.com>
2022-11-27 13:00:52 +0200

    front page and about page created

--- rlcrm/core/views.py
+++ rlcrm/core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # request is a paramater that contains information about the browser(post or get request)
 def index(request):
     return render(request, 'core/index.html')
+
+def about(request):
+    return render(request, 'core/about.html')
--- rlcrm/rlcrm/urls.py
+++ rlcrm/rlcrm/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from core.views import index
+from core.views import index, about
 
 urlpatterns = [
     path('', index, name='index'),
+    path('about/', about, name='about'),
     path('admin/', admin.site.urls),
 ]
